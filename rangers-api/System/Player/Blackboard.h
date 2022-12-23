24a64692ab60b8589418c38a35946ff3ca31a2f8
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2022-12-23 10:13:15 +0100

    Update GetBlackboardContent

--- rangers-api/System/Player/Blackboard.h
+++ rangers-api/System/Player/Blackboard.h
@@ -4,6 +4,7 @@ namespace app::player
 {
 	class BlackboardContent : public hh::fnd::ReferencedObject
 	{
+	public:
 		INSERT_PADDING(8);
 
 		virtual size_t GetNameHash();
@@ -15,13 +16,17 @@ namespace app::player
 		inline static void* ms_addrGetBlackboardContent = sigScan("\x48\x89\x5C\x24\x08\x48\x89\x6C\x24\x10\x48\x89\x74\x24\x18\x57\x48\x83\xEC\x20\x48\x8B\x79\x18\x8B\xEA", "xxxxxxxxxxxxxxxxxxxxxxxxxx", (void*)0x1407798C0);
 		inline static FUNCTION_PTR(BlackboardContent*, __fastcall, ms_fpGetBlackboardContent, ms_addrGetBlackboardContent, Blackboard*, size_t);
 
-	public:
-		csl::ut::MoveArray<BlackboardContent> Blackboards;
+		csl::ut::MoveArray<BlackboardContent*> m_Blackboards;
 
+	public:
 		template <typename T>
 		T* GetBlackboardContent()
 		{
-			return reinterpret_cast<T*>(ms_fpGetBlackboardContent(this, T::GetHashedName()));
+			for (auto* pBlackboard : m_Blackboards)
+				if (pBlackboard->GetNameHash() == csl::ut::StringMapOperation::hash(T::GetBlackboardName()))
+					return reinterpret_cast<T*>(pBlackboard);
+
+			return { nullptr };
 		}
 	};
 }
--- rangers-api/System/Player/BlackboardBattle.h
+++ rangers-api/System/Player/BlackboardBattle.h
@@ -4,6 +4,8 @@ namespace app::player
 {
 	class alignas(16) BlackboardBattle : public BlackboardContent
 	{
+		inline static const char* ms_pBlackboardName = "BlackboardBattle";
+
 	public:
 		INSERT_PADDING(32);
 		INSERT_PADDING(32); // csl::ut::MoveArray<>
@@ -13,9 +15,14 @@ namespace app::player
 		float PhantomRushMeter;
 		INSERT_PADDING(80);
 
-		static size_t GetHashedName()
+		virtual size_t GetNameHash() override
 		{
-			return csl::ut::StringMapOperation::hash("BlackboardBattle");
+			return csl::ut::StringMapOperation::hash(ms_pBlackboardName);
+		}
+
+		static const char* GetBlackboardName()
+		{
+			return ms_pBlackboardName;
 		}
 	};
 }
