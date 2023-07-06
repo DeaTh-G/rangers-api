2eb85b3135a5db59c936ad59e81d659421a65c62
Gabriel <34012267+HyperBE32@users.noreply.github.com>
2023-07-06 13:29:47 +0100

    [PlayerInformation] Fix IsGrounded flag for v1.30

--- rangers-api/System/Player/PlayerInformation.h
+++ rangers-api/System/Player/PlayerInformation.h
@@ -5,8 +5,7 @@ namespace app::player
 	class PlayerInformation : public hh::fnd::BaseObject
 	{
 	public:
-		INSERT_PADDING(726);
+		INSERT_PADDING(0x2E6);
 		bool IsGrounded;
-		INSERT_PADDING(578);
 	};
 }
