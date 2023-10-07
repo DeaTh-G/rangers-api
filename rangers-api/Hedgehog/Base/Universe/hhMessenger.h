134d4c02ab4ccb79f12491df05785fe771160202
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-10-07 12:53:57 +0200

    Forward declare hh::game::GameManager in Messenger

--- rangers-api/Hedgehog/Base/Universe/hhMessenger.h
+++ rangers-api/Hedgehog/Base/Universe/hhMessenger.h
@@ -1,8 +1,8 @@
 #pragma once
 
-namespace app
+namespace hh::game
 {
-	class GameDocument;
+	class GameManager;
 }
 
 namespace hh::fnd
@@ -11,7 +11,7 @@ namespace hh::fnd
 	{
 	private:
 		inline static void* ms_addrSendMessageImmToPlayer = sigScan("\x48\x89\x5C\x24\x08\x48\x89\x6C\x24\x10\x48\x89\x74\x24\x18\x57\x48\x83\xEC\x30\x4C\x89\xCF\x41\x0F\xB6\xE8", "xxxxxxxxxxxxxxxxxxxxxxxxxxx", (void*)0x146993E00);
-		inline static FUNCTION_PTR(void, __fastcall, ms_fpSendMessageImmToPlayer, ms_addrSendMessageImmToPlayer, Messenger*, app::GameDocument*, int, Message&);
+		inline static FUNCTION_PTR(void, __fastcall, ms_fpSendMessageImmToPlayer, ms_addrSendMessageImmToPlayer, Messenger*, hh::game::GameManager*, int, Message&);
 
 	public:
 		void* pUnk1;
@@ -24,7 +24,7 @@ namespace hh::fnd
 		virtual void fUnk4();
 		virtual int fUnk5() { return 1; }
 
-		void SendMessageImmToPlayer(app::GameDocument* in_pGameDocument, int in_playerNo, Message& in_rMessage)
+		void SendMessageImmToPlayer(hh::game::GameManager* in_pGameDocument, int in_playerNo, Message& in_rMessage)
 		{
 			ms_fpSendMessageImmToPlayer(this, in_pGameDocument, in_playerNo, in_rMessage);
 		}
