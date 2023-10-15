d876e362ee425cce6bf0f2f39c09cafb92673e96
Gabriel <34012267+HyperBE32@users.noreply.github.com>
2023-10-15 18:42:37 +0100

    StageInfo: use correct naming conventions for enums

--- rangers-api/System/Services/service_StageInfo.h
+++ rangers-api/System/Services/service_StageInfo.h
@@ -35,52 +35,61 @@ namespace app::level
 	class StageData : public hh::fnd::ReferencedObject
 	{
 	public:
-		enum Attributes
+		enum EAttributeFlags
 		{
-			Cyber = 0x01,
-			Minigame = 0x02,
-			Hacking = 0x03,
-			LastBoss = 0x04,
-			MasterTrial = 0x05,
-			Tutorial = 0x06,
-			Navmesh = 0x08,
-			HeightField = 0x09,
-			PointCloud = 0x0A,
-			Autosave = 0x0B,
-			Diving = 0x0D,
-			SideStep = 0x0E,
-			Athletic = 0x0F,
-			Boarding = 0x10,
-			Drift = 0x11,
-			SideView = 0x12,
-			Lava = 0x13,
-			Sonic = 0x14,
-			Tails = 0x15,
-			Amy = 0x16,
-			Knuckles = 0x17,
-			BattleRush = 0x18,
-			BossRush = 0x19,
-			Extra = 0x1A,
-			DeleteFallDeadCollision = 0x1B,
-			DeleteAirWallCollision = 0x1C,
-			DeleteGrindRail = 0x1D,
-			ChangeNewCollision = 0x1E,
-			RestrictDebris = 0x1F,
-			SpecialParryEffect = 0x20
+			EAttributeFlags_Cyber = 0x01,
+			EAttributeFlags_Minigame = 0x02,
+			EAttributeFlags_Hacking = 0x03,
+			EAttributeFlags_LastBoss = 0x04,
+			EAttributeFlags_MasterTrial = 0x05,
+			EAttributeFlags_Tutorial = 0x06,
+			EAttributeFlags_Navmesh = 0x08,
+			EAttributeFlags_HeightField = 0x09,
+			EAttributeFlags_PointCloud = 0x0A,
+			EAttributeFlags_Autosave = 0x0B,
+			EAttributeFlags_Diving = 0x0D,
+			EAttributeFlags_SideStep = 0x0E,
+			EAttributeFlags_Athletic = 0x0F,
+			EAttributeFlags_Boarding = 0x10,
+			EAttributeFlags_Drift = 0x11,
+			EAttributeFlags_SideView = 0x12,
+			EAttributeFlags_Lava = 0x13,
+			EAttributeFlags_Sonic = 0x14,
+			EAttributeFlags_Tails = 0x15,
+			EAttributeFlags_Amy = 0x16,
+			EAttributeFlags_Knuckles = 0x17,
+			EAttributeFlags_BattleRush = 0x18,
+			EAttributeFlags_BossRush = 0x19,
+			EAttributeFlags_Extra = 0x1A,
+			EAttributeFlags_DeleteFallDeadCollision = 0x1B,
+			EAttributeFlags_DeleteAirWallCollision = 0x1C,
+			EAttributeFlags_DeleteGrindRail = 0x1D,
+			EAttributeFlags_ChangeNewCollision = 0x1E,
+			EAttributeFlags_RestrictDebris = 0x1F,
+			EAttributeFlags_SpecialParryEffect = 0x20
 		};
 
-		enum Missions
+		enum ECyberModeFlags
 		{
-			Goal = 0x01,
-			Rank = 0x02,
-			Ring = 0x04,
-			RedRing = 0x05,
-			NumberRing = 0x06,
-			SilverMoonRing = 0x07,
-			HideGoal = 0x08,
-			ShadowTails = 0x09,
-			Mine = 0x0A,
-			Animal = 0x0B
+			ECyberModeFlags_LowGravity = 0x01,
+			ECyberModeFlags_TimeExtend = 0x02,
+			ECyberModeFlags_SpeedScale = 0x03,
+			ECyberModeFlags_Nitro = 0x04,
+			ECyberModeFlags_MaxSpeedChallenge = 0x05
+		};
+
+		enum EMissionFlags
+		{
+			EMissionFlags_Goal = 0x01,
+			EMissionFlags_Rank = 0x02,
+			EMissionFlags_Ring = 0x04,
+			EMissionFlags_RedRing = 0x05,
+			EMissionFlags_NumberRing = 0x06,
+			EMissionFlags_SilverMoonRing = 0x07,
+			EMissionFlags_HideGoal = 0x08,
+			EMissionFlags_ShadowTails = 0x09,
+			EMissionFlags_Mine = 0x0A,
+			EMissionFlags_Animal = 0x0B
 		};
 
 	public:
