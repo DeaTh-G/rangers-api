38056efd85a29a400cc318c1b7c971d96d1bf024
Gabriel <34012267+HyperBE32@users.noreply.github.com>
2023-10-18 18:37:50 +0100

    PlayerInformation: map character index

--- rangers-api/System/Player/PlayerInformation.h
+++ rangers-api/System/Player/PlayerInformation.h
@@ -7,8 +7,10 @@ namespace app::level
 	public:
 		INSERT_PADDING(8);
 		int PlayerHandle{};
-		INSERT_PADDING(730) {};
+		INSERT_PADDING(4);
+		uint8_t CharacterIndex{};
+		INSERT_PADDING(726);
 		bool IsGrounded{};
-		INSERT_PADDING(592) {};
+		INSERT_PADDING(592);
 	};
 }
